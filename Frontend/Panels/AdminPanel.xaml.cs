12e5ba86b901685d6a33bb3183cc81d39bfd8a57
jankr02 <jan.krueger4@de.bosch.com>
2023-12-20 11:02:49 +0100

    Final folder structure change for merge

---
+++ Frontend/Panels/AdminPanel.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MesseauftrittDatenerfassung_UI
+{
+    /// <summary>
+    /// Interaction logic for AdminPanel.xaml
+    /// </summary>
+    public partial class AdminPanel : Window
+    {
+        public AdminPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == textBox.Tag.ToString())
+            {
+                textBox.Text = string.Empty;
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = textBox.Tag.ToString();
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Close();
+            mainWindow.Show();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox selectedCheckBox = sender as CheckBox;
+            UpdateCheckBoxes(selectedCheckBox, true);
+            UpdateTextBoxVisibility(selectedCheckBox, true);
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox selectedCheckBox = sender as CheckBox;
+            UpdateCheckBoxes(selectedCheckBox, false);
+            UpdateTextBoxVisibility(selectedCheckBox, false);
+        }
+
+        private void UpdateCheckBoxes(CheckBox selectedCheckBox, bool isChecked)
+        {
+            foreach (ComboBoxItem item in FilterComboBox.Items)
+            {
+                CheckBox checkBox = (item.Content as CheckBox);
+                if (checkBox != null && checkBox != selectedCheckBox)
+                {
+                    checkBox.IsChecked = false;
+                }
+            }
+        }
+
+        private void UpdateTextBoxVisibility(CheckBox selectedCheckBox, bool isChecked)
+        {
+            // Setze alle Textfelder auf unsichtbar
+            FilterIdTextBox.Visibility = Visibility.Collapsed;
+            FilterIdBorder.Visibility = Visibility.Collapsed;
+            FilterNameTextBox.Visibility = Visibility.Collapsed;
+            FilterNameBorder.Visibility = Visibility.Collapsed;
+
+            // Wenn isChecked, mache das ausgewählte Textfeld sichtbar
+            if (isChecked)
+            {
+                if (selectedCheckBox.Tag.ToString() == "IdCheckBox")
+                {
+                    FilterIdTextBox.Visibility = Visibility.Visible;
+                    FilterIdBorder.Visibility = Visibility.Visible;
+                }
+                else if (selectedCheckBox.Tag.ToString() == "NameCheckBox")
+                {
+                    FilterNameTextBox.Visibility = Visibility.Visible;
+                    FilterNameBorder.Visibility = Visibility.Visible;
+                }
+                else if (selectedCheckBox.Tag.ToString() == "BothCheckBox")
+                {
+                    FilterIdTextBox.Visibility = Visibility.Visible;
+                    FilterIdBorder.Visibility = Visibility.Visible;
+                    FilterNameTextBox.Visibility = Visibility.Visible;
+                    FilterNameBorder.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+    }
+}
---
+++ Frontend/Panels/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MesseauftrittDatenerfassung.MesseauftrittDatenerfassung;
+
+namespace MesseauftrittDatenerfassung_UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CameraAPI cameraApi = new CameraAPI();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == textBox.Tag.ToString())
+            {
+                textBox.Text = string.Empty;
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = textBox.Tag.ToString();
+            }
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            watermarkTextBlock.Visibility = string.IsNullOrEmpty(passwordBox.Password) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void Image_Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Rufen Sie die Methode zum Starten der Kamera und zum Aufnehmen eines Bildes auf
+            byte[] imageBytes = cameraApi.CaptureImage();
+            if (imageBytes != null)
+            {
+                // Konvertieren Sie das Byte-Array in ein Bild und zeigen Sie es an
+                using (MemoryStream ms = new MemoryStream(imageBytes))
+                {
+                    var image = new BitmapImage();
+                    image.BeginInit();
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.StreamSource = ms;
+                    image.EndInit();
+
+                    // Assign the image source to your image control
+                    personalImage.Source = image;
+                }
+            }
+        }
+
+        private void OpenAdminPanel_Click(object sender, RoutedEventArgs e)
+        {
+            AdminPanel adminPanelWindow = new AdminPanel();
+            this.Close();
+            adminPanelWindow.Show(); // Show the Admin Panel window
+        }
+
+    }
+
+    public class PasswordLengthToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value != null && value is int && (int)value > 0) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
